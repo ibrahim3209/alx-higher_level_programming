de145370e26d5e8c385c6781150f43b6a2ba2276
ibrahim3209 <123ibrahimim@gmail.com>
2022-12-01 06:22:51 -0800

    my first python project

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+
