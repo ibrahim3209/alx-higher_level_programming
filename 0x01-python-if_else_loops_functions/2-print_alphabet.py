097864159e7e85dc45aa75b6f8d9d98daf10807d
ibrahim3209 <123ibrahimim@gmail.com>
2022-12-01 10:14:42 -0800

    0x01-python-if_else_loops_functions

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# 2-print_alphabet.py
+
+
+"""Print the alphabet in lowercase, not followed by a new line."""
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pric
